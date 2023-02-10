865083d30d5d10328cb30fba13605f018cf5ff1f
len <1059779749@qq.com>
2023-02-10 11:58:34 +0800

    初始化

---
+++ src/main/java/com/glzt/TestUtil.java
@@ -0,0 +1,20 @@
+package com.glzt;
+
+/**
+ * @author len
+ * @date 2023/2/10
+ * @Email 1059779749@qq.com
+ * @Description
+ */
+public class TestUtil {
+
+    public static Integer add(Integer a, Integer b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        Integer add = add(1, 2);
+        System.out.println(add);
+    }
+
+}
---
+++ src/main/java/com/glzt/UtilsMain.java
@@ -0,0 +1,10 @@
+package com.glzt;
+
+/**
+ * @author len
+ * @date 2023/2/10
+ * @Email 1059779749@qq.com
+ * @Description
+ */
+public class UtilsMain {
+}
